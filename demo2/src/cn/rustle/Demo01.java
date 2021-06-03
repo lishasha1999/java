1536fb5fd6f58caf20ffce4d0463397f799c1093
lishasha1999 <84852993+lishasha1999@users.noreply.github.com>
2021-06-03 22:22:40 +0800

    学习画笔 demo2

--- demo2/src/cn/rustle/Demo01.java
+++ demo2/src/cn/rustle/Demo01.java
@@ -2,6 +2,7 @@ package cn.rustle;
 
 /*引用cn.tedu.util.App*/
 import cn.tedu.util.App;
+import java.awt.*;
 
 public class Demo01 extends App{
     public static void main(String[] args){
@@ -10,5 +11,24 @@ public class Demo01 extends App{
         Demo01 demo=new Demo01();
         //使用start方法
         demo.start();
+    }
+
+    /*封装好的绘制方法，来自于APP类
+     *Graphics2D是画笔对象，封装了很多绘画功能
+     * ;表示一行代码的结束
+     */
+    @Override//重写的标志
+    public void painting(Graphics2D g) {
+        /*创建颜色 RGB格式
+        * new Color();创建颜色类
+        */
+        g.setColor(new Color(255,0,255));//给画笔设置颜色
+        g.setFont( new Font("黑体",Font.BOLD,30));//给画笔设置字体
+        //绘制字符串
+        g.drawString("NEW DEMO",90,50);
+
+        g.setColor(new Color(255,0,0));
+        g.setFont( new Font("新宋体",Font.BOLD,30));
+        g.drawString("DEMO END",150,200);
     }
 }
